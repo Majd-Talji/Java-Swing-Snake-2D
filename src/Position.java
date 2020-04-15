0035402cb40688a7b972632d9d7c85194749a258
Majd-Talji <53352101+Majd-Talji@users.noreply.github.com>
2020-04-15 14:34:01 +0200

    first commit

---
+++ src/Position.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Majd Talji <en.majd.talji@gmail.com>
+ */
+public class Position {
+    int x, y;
+    
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
